567a7aafc4074710eec7ce54f26f1760d5ae4568
Juns Bob <p_juns@kakao.com>
2021-07-01 12:55:20 +0900

    Modify configurations

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import axios from 'axios';
+import { API_URI } from '../config';
 
+// styles
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 
-axios.defaults.baseURL = process.env.API_URI;
+axios.defaults.baseURL = API_URI;
 
 render(
   <>
