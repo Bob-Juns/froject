8029164704b852aa0c04ba6ccf417083f1cae57f
Juns Bob <p_juns@kakao.com>
2021-07-01 14:15:15 +0900

    Add dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import axios from 'axios';
-import { API_URI } from '../config';
 
 // styles
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 
-axios.defaults.baseURL = API_URI;
+axios.defaults.baseURL = process.env.API_URI;
 
 render(
   <>
