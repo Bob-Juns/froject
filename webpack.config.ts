91857ddf7cc338b31c3001c83d94a6e1bc1a96c6
Juns Bob <p_juns@kakao.com>
2021-08-15 16:08:59 +0900

    Fix webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,75 +6,83 @@ const Dotenv = require('dotenv-webpack');
 
 const production = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  mode: process.env.NODE_ENV,
-  devtool: production ? 'eval' : 'inline-cheap-module-source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.html'],
-    alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@layouts': path.resolve(__dirname, 'src/layouts'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
+module.exports = () => {
+  require('dotenv').config({
+    path: path.resolve(__dirname, '.env'),
+  });
+
+  return {
+    mode: process.env.NODE_ENV,
+    devtool: production ? 'eval' : 'inline-cheap-module-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.html'],
+      alias: {
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@routes': path.resolve(__dirname, 'src/routes'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+      },
     },
-  },
-  entry: {
-    main: ['./src/index.tsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: '/node_modules/',
-        use: ['ts-loader'],
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[name].[ext]?[hash]',
+    entry: {
+      main: ['./src/index.tsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: '/node_modules/',
+          use: ['ts-loader'],
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader',
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                name: '[name].[ext]?[hash]',
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    output: {
+      path: production
+        ? path.resolve(__dirname, 'build')
+        : path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        favicon: path.resolve(__dirname, 'public/favicon.ico'),
+      }),
+      new CleanWebpackPlugin(),
+      new Dotenv(),
+      new webpack.DefinePlugin({
+        'process.env.API_URI': JSON.stringify(process.env.API_URI),
+        'process.env.GA_TRACKING_ID': JSON.stringify(
+          process.env.GA_TRACKING_ID,
+        ),
+      }),
     ],
-  },
-  output: {
-    path: production
-      ? path.resolve(__dirname, 'build')
-      : path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-      favicon: path.resolve(__dirname, 'public/favicon.ico'),
-    }),
-    new CleanWebpackPlugin(),
-    new Dotenv(),
-    new webpack.DefinePlugin({
-      'process.env.API_URI': JSON.stringify(process.env.API_URI),
-      'process.env.GA_TRACKING_ID': JSON.stringify(process.env.GA_TRACKING_ID),
-    }),
-  ],
-  devServer: {
-    contentBase: './dist',
-    writeToDisk: true,
-    historyApiFallback: true,
-    port: 3003,
-  },
+    devServer: {
+      contentBase: './dist',
+      writeToDisk: true,
+      historyApiFallback: true,
+      port: 3003,
+    },
+  };
 };
